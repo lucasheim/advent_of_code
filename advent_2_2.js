e4a6129aec5435036d4053c0c0854432a3606fff
Heim <lucas.heim@sap.com>
2018-12-06 00:00:42 -0200

    Improving day two part two (half the time now!)

--- advent_2_2.js
+++ advent_2_2.js
@@ -1,31 +1,28 @@
-var input = "wxlnjevbfozadyiqpuzkrhstkg,wxlsjivbfodamyiqpuzcxhstkg,wxlnjevbfouammnqpuzcrhstkg,wxlnjevbfobwmyiqpuzprhstkg,wxlnjefbfodamyiqpuzcnustkg,wxlnjevbfodakyyupuzcrhstkg,wxlnjejbfodafynqpuzcrhstkg,wxlnjevbfodomyifptzcrhstkg,wxlnjevbfodamyiwcuzcrhstkz,wxlnjevbfofamyiqpuznrhstxg,wxlnjevbfodamyiqbupcrxstkg,wxjnjevbfodxmyeqpuzcrhstkg,xwlnjevbfosamyiqpuzcrhstkg,fxlnjevbfodrmyiqpuzcrbstkg,wxlnjevpfodamyiqquzzrhstkg,wwlnjenbfodawyiqpuzcrhstkg,wxrnjevbfodamyiqpuzlrhstrg,welnjeqbfodkmyiqpuzcrhstkg,walnjfvbfodamyiqpuzcrhwtkg,wdlnjevbfodamriqpuzjrhstkg,wxlnjevbfodmnyizpuzcrhstkg,wxlnjevbfodgmyiqpuxcrjstkg,wxlnjevbfkdamyiqpudcrestkg,wxlyjevbfodamyiqpuzcehstkj,wxlnjevamodamyiqpuzcrhatkg,fxlnqevsfodamyiqpuzcrhstkg,wqlnjevbfodanyiqvuzcrhstkg,wxlnjevbfoiamyzqpuzcrnstkg,wxlnjevbfodamyiqpuacrhsjig,wxlnjuvbfodzmyvqpuzcrhstkg,kxlnjevbfolamviqpuzcrhstkg,wxlnjesbfldamyiqpuycrhstkg,nxltjevbfodgmyiqpuzcrhstkg,ojlnjevbfooamyiqpuzcrhstkg,wxlnjevbfodaffiqpugcrhstkg,cxlnievbfodamyuqpuzcrhstkg,wxlouevbjodamyiqpuzcrhstkg,wafnjevbfhdamyiqpuzcrhstkg,wxlnjevbfxdamrpqpuzcrhstkg,wxlnjepffodamyiqphzcrhstkg,wxlnkevbfohamciqpuzcrhstkg,wzlnmevdfodamyiqpuzcrhstkg,wxlzjtvbfodamyiqpuzcrhstkd,gxlnjehbfojamyiqpuzcrhstkg,wxlnjeoqfodamyiqprzcrhstkg,nxllvevbfodamyiqpuzcrhstkg,wxlnjevtfomamyiqpurcrhstkg,sxlnjevafodamyikpuzcrhstkg,wxlnjevbfodamyfqpuzcyhztkg,wxlnjevbfodamyiqpulnrhstkh,wxlnwevbfodumyiqpuzqrhstkg,wxldjevbfodamyiqpzzcrhstkk,jxlnjevbfodamyiqphzcrnstkg,fxlnjeibfodcmyiqpuzcrhstkg,wxlnjevufodamyiqpnzcrhstkk,wglnjevbfodamyiqpuzcesstkg,wxlvjevbdodamyiqpuzcrhstkc,wxlnjevbfodabyicpuzcrhstkl,wxlnjevbfodamyiqpizcrhstvt,wolnjevbfodawyiqiuzcrhstkg,wxlyjevbfodamyuqpxzcrhstkg,wxlijevbfodamyikpuzyrhstkg,wxennevbfodamyiqpuzcrtstkg,wxlnjevbyodamyuqpwzcrhstkg,wxlnjevbfoiomyiqpuzcrhsteg,wxlnjehbrodamyiqpuicrhstkg,xxlnjevufodamyiqbuzcrhstkg,wxlojevbfodamyiqpezcrhatkg,wxljjevbfolamuiqpuzcrhstkg,wxlnjevbfodamyiqruzcrhstpi,wxlnjevbfomamyiqjnzcrhstkg,wxlnjevbfodahyiqzuzcrhstpg,wxtnjevbfodamyiqpuzcrhsdrg,wxlnjevbfodamynrpuzcrhstkz,wxlqjevefqdamyiqpuzcrhstkg,wxlnjevbfmdamyiqnuzckhstkg,wxlnjevbfodajyiqprzcrjstkg,wxlnjqvbhodamyidpuzcrhstkg,wxlnjhvbfodamriqpuzcchstkg,wglnjevbfodamyiqpupfrhstkg,wulnjevdfodamyiqpuzcrhsteg,vxlojevbfodamyiqpuzcrhstsg,wxlnjvvbfodamiiqpuzcrhttkg,wxlnjevbfodabyiqpuzzrhetkg,wxhnjevbfodamyiqpuwcrsstkg,wslzjbvbfodamyiqpuzcrhstkg,rxlnjevbfodhmyiqpupcrhstkg,wxlnjevbfhdamyiqpuvcrhskkg,wxlrjevbxodamyiqpuzcrhstag,wxlsbevbfodammiqpuzcrhstkg,wxlnjzvbfodemyiqpmzcrhstkg,wxlnoevbfodgmyiqpuzbrhstkg,wxlnjefbfodamyinpuzcrhwtkg,bxlnjevbfwdamyiqpuocrhstkg,cxlnjevbjodamyiqpuzcrhslkg,wflnjevbforemyiqpuzcrhstkg,wxlmjeoboodamyiqpuzcrhstkg,wxlnjevbfadaiyiqpuzcrhutkg,wxlnmevbfodamyyqpuzcrjstkg,wxlnjovbfodamyippjzcrhstkg,wxlnjmvbfodamyiqpszcrhsbkg,wxlnjeebfodamyicpuxcrhstkg,wxlnrehbfodamyiqpuzcrhytkg,wxlnjevbfogamyiqwurcrhstkg,wxlujevbnodamyiqpuzcrhstng,wxlnoenofodamyiqpuzcrhstkg,wxsnjevbfsdamyiqsuzcrhstkg,wxlnjevwfodamyiqpuzxrhqtkg,wxlnjevbnodamyiqxulcrhstkg,wxlijetpfodamyiqpuzcrhstkg,wxlnjzvbfidamyiqpuzcrbstkg,wxlnjevefodavyiqpuzcthstkg,wxlnjevbfozamyiqpurcrbstkg,wxlnjfvpfodamyiqpuzcrhntkg,wxlnjevbfvdamyiqvuzcrhqtkg,wilejevbfodamyilpuzcrhstkg,wxlnhevbfodamtiqpuzcrhstke,wxlwjevbfodahyiqppzcrhstkg,wxlnjevbfodamyuqpuzwrrstkg,xxsnjevbfodamyiqpuzcrhstkl,wglnjevbdodamyaqpuzcrhstkg,wxlnjefbwodamyiqpuzcrhsykg,wxwnjevbfodamyiqpuzcrhpckg,wxlnjuvbfidamyiqpuzczhstkg,wxlnzhybfodamyiqpuzcrhstkg,wxunjevufodamyiqpuzcrhspkg,wxunjevbfodcmyiqpuzcrhstcg,wxlnjevbfodhwyiqpuxcrhstkg,wxlnjevtfodasyiqpuzcrhstkb,wxlvjevbfqdamyiqprzcrhstkg,sxlnjevbfodamyiqplzcrhstkq,wxlnlevbfodamyiqpuzcrpstka,wxlnjevbfodaiyiqovzcrhstkg,wxlntevbfodamyiqpuzcrkntkg,wxlnjevbfodsmyiqpuzcrhstir,wxlnnevbfodaoyiqpuzmrhstkg,xxlnjevbfodamyinpnzcrhstkg,wxlnjedefodamyigpuzcrhstkg,wxlnxeabfodamyiqpnzcrhstkg,wxlnxevbfodpmtiqpuzcrhstkg,wxlnjevnfodamyiqpuzcuhqtkg,wxlnjevbfodakyiqluzcrhstmg,wxlnjevbaodamoiqpyzcrhstkg,wwlnjevbfoaajyiqpuzcrhstkg,wxlnjevbfedamyiqpuzcrhsang,wxlwjevbfodamyiqpuzcrmdtkg,wxlnjevbhodamyiqpmzxrhstkg,wxlnjevbfodamyiqpuzzrhwtkj,wxlnjevbfpdvmyiqpuzzrhstkg,wxlnjegcfodamyiqpxzcrhstkg,fxlnjevbfodamyiqpuzcrhstat,wxlnjevbfodcmybqpuzcrkstkg,wxlnjevbfodamiiqpuzrrhstxg,wxvnjevifodamdiqpuzcrhstkg,wxltjevbfodamyiqpuzmrhktkg,wxlnjevbfobaaygqpuzcrhstkg,wmlnjevbfodamyiqpuycrhsukg,wxlnjevboodamyiqpuzcrhuhkg,wxlnjevgfodaqyiqpuzcghstkg,wxlnjevjnodamyiqpuzcrhstke,wclnjevbfodamyiqpuncchstkg,wxlnjevbfndamyxqpuzcshstkg,rxldjevbfodamyiqpuvcrhstkg,wxlnwevbfodamywqpuzrrhstkg,ixlnjevbqodpmyiqpuzcrhstkg,wxlnjlvbfocamyiqpuzgrhstkg,wxlnjevffodamyiqnuzcrhutkg,wxlajevbfodamyiqpuccrhshkg,vwlnjevbfodamyiqpuzcrhstky,wxlajevbfodamyiqpuzcfhstkl,wxlnjevbfodamniqouzcrhstko,wxlnjevbfodamyiqpuzqrhsqka,wxlnjeybfodamyiqpuzclhsnkg,wxbnjlvbfoyamyiqpuzcrhstkg,wxbnjevbfodemyiqpuzcrhstkj,wxlnbefbfodamyiqpkzcrhstkg,wxlnjvvbyodamyitpuzcrhstkg,jxlnjevbfopamyiqpuzprhstkg,wxlnjevbfodaeyiupxzcrhstkg,wnlnjevbfodamyqqpuzcrhstcg,wxlxzuvbfodamyiqpuzcrhstkg,wxlnjevbcodymyiqpuzcrhstke,wxlnjezbfodamynqpuvcrhstkg,wxlnjevbfodamyxlpuzcyhstkg,wxlnjevbffdaiyiqpuzirhstkg,wxlnjevbfodymyiqwuzcrhstfg,wxlnzevbfodscyiqpuzcrhstkg,hxlnjevbfodamyawpuzcrhstkg,welnjevbfodamciqplzcrhstkg,wxlnjeqbfodawyiqpuzkrhstkg,wxlnjelbfodamviqpuzckhstkg,wxlneevjfodamyiqpuzcrhstkd,wxlnjevbfodamyaqpuytrhstkg,wxlnjpvyfodamyiqpuzcshstkg,wxlnjevbfodmbyiqpuzcrhstkp,wxlnjegbfodamdiqpuzcrhstdg,wmlnjevbfodamyiqpuecrhsukg,wxlnjevbfodamyiqpuocrhwtjg,jxfnwevbfodamyiqpuzcrhstkg,wxlnjevffodamyiqpurcrhstkd,wxlnjevbfofamyiqpuzcrhsmkt,wxlnjevbfodmmyiqpuzcrdsttg,axlnjevbfodgmyiqpuzerhstkg,wxtnjevbfodamyifpuwcrhstkg,wxlgjevbfodamyiqpuzrrhvtkg,wxlnjevbfouamyeqfuzcrhstkg,wxlnjevbfmxamyiqpuzcahstkg,wxlnjevffoxamyiqpuecrhstkg,wxlnyevbfodamyiqttzcrhstkg,bxlnjevbfodzmysqpuzcrhstkg,wxlnjevbfodayyiqpuzcrherkg,yxlnjevbfodayyiqpuzcrwstkg,wllnjevbfodambiqpuzurhstkg,wxlnjevbfsdamyiqpuqcrhstkh,wxlcjevbfodamyiqcuzcxhstkg,wxlnjevbfodayticpuzcrhstkg,wxltzevbfodamyiqpzzcrhstkg,wxlnjevbfodamgiqpuzcphstng,wxlnjevbfqdfmziqpuzcrhstkg,wxlnaevbfodamyiqpuzcfustkg,wxlnjevbfodamyxqxuzcrhstdg,wxlnjevkbodamyiqpufcrhstkg,whlnjevbfodauyiqputcrhstkg,wxlnjevbfodamyiephzcrhsnkg,wxlnjevbfodfmoiqpuzcrhstkf,wxlnjevbfodamyiqxuzaxhstkg,wxlnjevtfotamyiqpuzcrhsttg,wxlgjevbfodamyiqpuhcrostkg,dxlnjtvbfodamyiqpuzcshstkg,wxlfjevbfodumyiqppzcrhstkg,wxlnzevbfodamyiqpuzqrhstkx,wflnjevbfodamyiqpurcrhsthg,wxlnjevbfodzfyiqpuzcrjstkg,wxlnjevbfrdamviqpuzmrhstkg,wnlngevmfodamyiqpuzcrhstkg,walzjevbfodamyiqpuzcrhsjkg,wqlnjevbfodamyiqpuzcshslkg,wxlnjevkfodfmyiepuzcrhstkg,wxgnjehbfodamyhqpuzcrhstkg,wxlnjevbfodamyiqfuacrostkg,wxlnjexbfodamyiwpuzcrqstkg,wxlntevafodamyiqpuzcrhsnkg,wxvnjevbfodamyiqpuzcvistkg,mxlnjeebfodamyiqpuzcrhsgkg,wxlnjevyfodamyiqpuzcrhdtkf,wxlnjcvbfodamyicpuzcrhsckg,wxlnjekbfodlmyiqpuzcthstkg,wxlnjvvbfodamyiopuzcrhstqg,wxlnjevbsodamyiqpuhcrhstwg,wxxnjevufodamyiqruzcrhstkg";
+const InputReader = require("./InputReader");
+const AnswerPrinter = require("./AnswerPrinter");
+
+const input = InputReader.readFile("inputs/input_2.txt");
 //input = "abcde,fghij,klmno,pqrst,fguij,axcye,wvxyz";
 
-var words = input.split(",");
-var rightWords = {};
-
-words.forEach((word, index) => {
-  var otherWords = words.slice(index + 1);
-  otherWords.forEach((otherWord) => {
-    var differentLetters = 0;
-    for (var i = 0; i < word.length ; i++) {
-      if (word.charAt(i) !== otherWord.charAt(i)) {
-        differentLetters++;
+function findCommonBoxIdLetters(words) {
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    for (let j = i + 1; j < words.length; j++) {
+      const otherWord = words[j];
+      let differentLetters = 0;
+      let differentLetterIndex = -1;
+      for (let k = 0; k < word.length; k++) {
+        if (word.charAt(k) !== otherWord.charAt(k)) {
+          differentLetters++;
+          differentLetterIndex = k;
+        }
+        if (differentLetters > 1) break;
       }
-      if (differentLetters > 1) break;
+      if (differentLetters === 1) {
+        return word.substr(0, differentLetterIndex) + word.substr(differentLetterIndex + 1);
+      }
     }
-    if (differentLetters === 1) {
-      rightWords[word] = otherWord;
-    }
-  });
-});
-var result = "";
-
-Object.keys(rightWords).forEach((word) => {
-  for (var i = 0; i < word.length; i++) {
-   if (word.charAt(i) == rightWords[word].charAt(i)) {
-      result += word.charAt(i);
-   } 
   }
-});
-console.log(result);
+}
+
+AnswerPrinter.printAnswerWithTime(findCommonBoxIdLetters, input.split("\n"));
