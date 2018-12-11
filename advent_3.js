c2cbfb97f11ccc897e07679a4b9b2bee11f446b8
Heim <lucas.heim@sap.com>
2018-12-11 14:44:10 -0200

    regex named groups ftw

--- advent_3.js
+++ advent_3.js
@@ -5,18 +5,16 @@ const input = InputReader.readFile("inputs/input_3.txt");
 //input = "#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 4,4: 2x2";
 
 function countOverlappingFabric(boxes) {
-  const pattern = /\#([\d]*) @ ([\d]*),([\d]*): (\d*)x(\d*)/g;
+  const pattern = /\#([\d]*) @ (?<yValue>[\d]*),(?<xValue>[\d]*): (?<widthValue>\d*)x(?<heightValue>\d*)/;
   const matrix = {};
   let count = 0;
 
   for (let box of boxes) {
-    const regex = new RegExp(pattern);
-    const matches = regex.exec(box);
-    const id = matches[1];
-    const x = Number(matches[3]) + 1;
-    const y = Number(matches[2]) + 1;
-    const width = Number(matches[4]);
-    const height = Number(matches[5]);
+    const { xValue, yValue, widthValue, heightValue } = box.match(pattern).groups;
+    const x = +xValue + 1;
+    const y = +yValue + 1;
+    const width = +widthValue;
+    const height = +heightValue;  
 
     for (let i = x; i < x + height; i++) {
       matrix[i] = matrix[i] ? matrix[i] : {};
